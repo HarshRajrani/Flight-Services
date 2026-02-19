b462f5b6624db7e9192cf56662dacc6b2b246ff8
HarshRajrani <harshrajrani717@gmail.com>
2026-02-19 17:09:09 +0530

    logger update

--- src/config/logger-config.js
+++ src/config/logger-config.js
@@ -1,18 +1,10 @@
-// import {createLogger, format, transports,} from 'winston';
-// const {combine, timestamp, printf, colorize, errors, } = format;
+import winston from "winston";
 
-// const customFormat = printf(({level, message, timestamp, label}) => {
-//     return `${timestamp}:${level}: ${message}`;
+const logger = winston.createLogger({
+  level: "info",
+  transports: [
+    new winston.transports.Console()
+  ]
+});
 
-// });
-//  const logger = createLogger({
-//     format: combine(
-//         timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-//         customFormat,
-//     ),
-//     transports: [new transports.Console(),
-//         new transports.File({filename: 'logs/error.log'}) //file name is a property of transports.File
-//     ],
-//  });
-
-//  export default logger;
+export default logger;
