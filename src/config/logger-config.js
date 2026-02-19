b04ca61024dd14122c936cea1f78b6a858adf688
HarshRajrani <harshrajrani717@gmail.com>
2026-02-19 17:05:48 +0530

    logger-removal

--- src/config/logger-config.js
+++ src/config/logger-config.js
@@ -1,18 +1,18 @@
-import {createLogger, format, transports,} from 'winston';
-const {combine, timestamp, printf, colorize, errors, } = format;
+// import {createLogger, format, transports,} from 'winston';
+// const {combine, timestamp, printf, colorize, errors, } = format;
 
-const customFormat = printf(({level, message, timestamp, label}) => {
-    return `${timestamp}:${level}: ${message}`;
+// const customFormat = printf(({level, message, timestamp, label}) => {
+//     return `${timestamp}:${level}: ${message}`;
 
-});
- const logger = createLogger({
-    format: combine(
-        timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-        customFormat,
-    ),
-    transports: [new transports.Console(),
-        new transports.File({filename: 'logs/error.log'}) //file name is a property of transports.File
-    ],
- });
+// });
+//  const logger = createLogger({
+//     format: combine(
+//         timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+//         customFormat,
+//     ),
+//     transports: [new transports.Console(),
+//         new transports.File({filename: 'logs/error.log'}) //file name is a property of transports.File
+//     ],
+//  });
 
- export default logger;
+//  export default logger;
