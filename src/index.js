b9f1d4da0a06eab278d2dbd58ad45891d11299ed
HarshRajrani <harshrajrani717@gmail.com>
2026-02-19 16:47:10 +0530

    vercel.json added

--- src/index.js
+++ src/index.js
@@ -1,41 +1,23 @@
 import express from 'express';
-import  {PORT} from './config/server-config.js';
-// import logger from './config/logger-config.js';
 import swaggerDocs from './config/swagger.js';
-import  apiRoutes  from './routes/index.js';
+import apiRoutes from './routes/index.js';
 import cors from 'cors';
 
 const app = express();
-console.log("Before swagger");
-swaggerDocs(app);
-console.log("After swagger");
 
+swaggerDocs(app);
 
 const corsOptions = {
-    origin: 'http://localhost:8080',
+  origin: '*', // ⚠ Vercel ke liye temporarily open rakho
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
-   
-}
-
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 
+app.use('/api', apiRoutes);
 
-
-
-app.use('/api',apiRoutes);
-
-
-app.listen(PORT, async ()=>{
-    console.log(`Server is running on port : ${PORT}`);
-    console.log(`Swagger is running on ${PORT}`);
-    
-   
-    
-});
-
-
+export default app;
